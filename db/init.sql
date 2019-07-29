a6015b82e188d1739e9ae58cf7909dc78e274d9d
thomas brown <thomasbrown9101@gmail.com>
2019-07-29 12:03:40 -0700

    created databases and joined them

--- db/init.sql
+++ db/init.sql
@@ -12,3 +12,36 @@ create table users(
 
 insert into users(username, password, email)
 values('tbrown', 'test', 'tbrown@gmail.com');
+
+create table playlist(
+    playlist_id serial primary key,
+    playlist_name varchar(40) not null,
+    user_id integer references user(user_id)
+);
+
+insert into playlist(playlist_name, user_id)
+values('my first playlist', 1);
+
+create table songs(
+    song_id serial primary key,
+    name varchar(200) not null,
+    artist varchar(200) not null
+);
+
+insert into songs(name, artist)
+values('babyshark', 'pinkfong');
+
+create table songs_in_playlist(
+    playlist_song_id serial primary key,
+    song_id integer references songs(song_id),
+    playlist_id integer references playlist(playlist_id)
+);
+
+
+
+-- grabs playlist name and song name and artist and joins it onto one table
+select * from songs
+join songs_in_playlist
+on songs.song_id = songs_in_playlist.song_id
+join playlist
+on playlist.playlist_id = songs_in_playlist.playlist_id;
