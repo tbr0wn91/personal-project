673deb75b3851d7205c27366ba2c0667f2dbf251
thomas brown <thomasbrown9101@gmail.com>
2019-08-07 15:43:01 -0700

    created responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,74 +1,105 @@
-import React, {Component} from 'react';
-import {Route, Switch, NavLink, withRouter} from 'react-router-dom';
-import Login from './components/Home/Login';
-import Register from './components/Home/Register';
-import Profile from './components/Profile/Profile';
-import CreatePlaylist from './components/Playlist/CreatePlaylist';
-import {connect} from  'react-redux';
-import {getUser} from './redux/reducer'
+import React, { Component } from "react";
+import { Route, Switch, NavLink, withRouter } from "react-router-dom";
+import Login from "./components/Home/Login";
+import Register from "./components/Home/Register";
+import Profile from "./components/Profile/Profile";
+import CreatePlaylist from "./components/Playlist/CreatePlaylist";
+import { connect } from "react-redux";
+import { getUser } from "./redux/reducer";
 
-import './App.scss';
-import axios from 'axios';
-import DisplayPlaylist from './components/DisplayPlaylist/DisplayPlaylist';
+import "./App.scss";
+import axios from "axios";
+import DisplayPlaylist from "./components/DisplayPlaylist/DisplayPlaylist";
 
 class App extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggleSideBar: false
+    };
 
-    this.userSession = this.userSession.bind(this)
-    this.logout = this.logout.bind(this)
+    this.userSession = this.userSession.bind(this);
+    this.logout = this.logout.bind(this);
+    this.toggleSideBarFunc = this.toggleSideBarFunc.bind(this);
   }
 
-  componentDidMount(){
-    this.userSession()
+  componentDidMount() {
+    this.userSession();
   }
 
-
-  userSession(){
-    axios.get('/auth/user_session').then(res =>{
+  userSession() {
+    axios.get("/auth/user_session").then(res => {
       this.props.getUser(res.data);
-    })
+    });
   }
 
-
-  logout(){
-    axios.get(`/auth/logout`).then(res =>{
-      this.props.history.push('/');
-    })
+  toggleSideBarFunc() {
+    this.setState(prevState => {
+      return {
+        toggleSideBar: !prevState.toggleSideBar
+      };
+    });
   }
 
-  render(){
-    console.log(this.props)
+  logout() {
+    axios.get(`/auth/logout`).then(res => {
+      this.props.history.push("/");
+    });
+  }
+
+  render() {
+    console.log(this.props);
     return (
       <div>
         <header className="header-container">
-          <span>Devify</span>
-          <div>
-            <NavLink exact to='/' activeClassName='active'>Home</NavLink>
-            <NavLink exact to='/Register' activeClassName='active'>Register</NavLink>
-            <NavLink exact to='/Profile' activeClassName='active'>Profile</NavLink>
-            <button onClick={this.logout}>Log Out</button>
+          <div className="devify">Devify</div>
+          <div className="menu-container">
+            <button className="toggle-button" onClick={this.toggleSideBarFunc}>
+              { this.state.toggleSideBar ? 'Close Menu' : 'Open Menu'}
+            </button>
           </div>
-  
+          <nav className={this.state.toggleSideBar ? "show" : ""}>
+            <NavLink className="links" exact to="/" activeClassName="active">
+              Home
+            </NavLink>
+            <NavLink
+              className="links"
+              exact
+              to="/Register"
+              activeClassName="active"
+            >
+              Register
+            </NavLink>
+            <NavLink
+              className="links"
+              exact
+              to="/Profile"
+              activeClassName="active"
+            >
+              Profile
+            </NavLink>
+            <button className="mobile-logout" onClick={this.logout}>
+              Log out
+            </button>
+          </nav>
+          <button className="logout" onClick={this.logout}>
+            Log Out
+          </button>
         </header>
-      
-  
-  
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route path="/Register"component={Register}/>
-        <Route path="/Profile" component={Profile}/>
-        <Route path="/CreatePlaylist" component={CreatePlaylist}/>
-        <Route path="/DisplayPlaylist" component={DisplayPlaylist} />
-  
-      </Switch>
+
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route path="/Register" component={Register} />
+          <Route path="/Profile" component={Profile} />
+          <Route path="/CreatePlaylist" component={CreatePlaylist} />
+          <Route path="/DisplayPlaylist" component={DisplayPlaylist} />
+        </Switch>
       </div>
     );
   }
-  
 }
 
-function mapReduxToProps(reduxState){
+function mapReduxToProps(reduxState) {
   return reduxState;
 }
 
